19bd72f14919d9e8b17b6a56c0563cc651b349e3
Thiago Cordeiro <thiagcordeirodantas@gmail.com>
2022-10-24 17:04:42 -0300

    Biblioteca fs

--- Node.js_Criando_Sua_Primeira_Biblioteca/index.js
+++ Node.js_Criando_Sua_Primeira_Biblioteca/index.js
@@ -1,6 +1,11 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('Ola Mundo'))
-console.log('Olá Mundo São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e' )
-console.log('Olá Mundo São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e')
-console.log('Olá Mundo')    
+function PegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8'
+    fs.readFile(caminhoDoArquivo,encoding,(_,texto)=> {
+        console.log(chalk.red(texto))
+    })
+}
+
+PegaArquivo("./Arquivos/text.md")
