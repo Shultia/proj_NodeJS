263e4825fe27dfa74a5a5b5916adf00b409f4074
Thiago Cordeiro <thiagcordeirodantas@gmail.com>
2022-10-24 16:55:07 -0300

    Instalando dependencias

--- Node.js_Criando_Sua_Primeira_Biblioteca/index.js
+++ Node.js_Criando_Sua_Primeira_Biblioteca/index.js
@@ -1,1 +1,6 @@
-console.log('Olá Mundo')
+import chalk from 'chalk';
+
+console.log(chalk.blue('Ola Mundo'))
+console.log('Olá Mundo São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e' )
+console.log('Olá Mundo São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e')
+console.log('Olá Mundo')    
