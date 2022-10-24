ba5e9411ad62c9edda5a72400152c5d4349a657c
Thiago Cordeiro <thiagcordeirodantas@gmail.com>
2022-10-24 17:19:21 -0300

    Promises

--- Node.js_Criando_Sua_Primeira_Biblioteca/index.js
+++ Node.js_Criando_Sua_Primeira_Biblioteca/index.js
@@ -1,11 +1,25 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-function PegaArquivo(caminhoDoArquivo){
-    const encoding = 'utf-8'
-    fs.readFile(caminhoDoArquivo,encoding,(_,texto)=> {
-        console.log(chalk.red(texto))
-    })
+
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.blue(erro.code, 'nao ha arquivo no diretorio'));
 }
+
+function PegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8';
+    fs.promises
+    .readFile(caminhoDoArquivo,encoding)
+    .then((Text) =>console.log(chalk.green(Text)))
+    .catch((erro) => trataErro(erro))
+}
+
+// function PegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8'
+//     fs.readFile(caminhoDoArquivo,encoding,(_,texto)=> {
+//         console.log(chalk.red(texto))
+//     })
+// }
 
 PegaArquivo("./Arquivos/text.md")
